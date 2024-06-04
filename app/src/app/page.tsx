a2779755e6abedee71fb520f2fcc2ceaa4f7b6da
Elise Medeiros <79584555+Elise-16-medeiros@users.noreply.github.com>
2024-06-04 11:13:38 -0300

    feat: images commit

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,7 +1,9 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-   <main><h1>Welcome</h1></main>
-  );
+	return (
+		<main>
+			<h1 className="text-3xl">HOME PAGE</h1>
+		</main>
+	);
 }
---
+++ app/src/components/footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+			<footer>
+				<p className="text-sm text-muted-foreground">
+					&copy; {new Date().getFullYear()} All rights reserved
+				</p>
+			</footer>
+		);
+}
