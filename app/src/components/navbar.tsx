c4d779315eb032ece489c924d31e99c21dd13346
Elise Medeiros <79584555+Elise-16-medeiros@users.noreply.github.com>
2024-06-04 12:23:06 -0300

    feat: framermotion installation

--- app/src/components/navbar.tsx
+++ app/src/components/navbar.tsx
@@ -1,5 +1,41 @@
+'use client'
+
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+
 export default function Navbar() {
-    return (
-        <nav>Navbar</nav>
-    )
+	const [open, setOpen] = useState(false);
+	const toggleMenu = () => {
+		setOpen((prevOpen) => !prevOpen)
+	};
+
+  return (
+    <header>
+      <nav className="flex w-full items-center justify-between border-b-2 border-[#EAE8F3] p-5 lg:px-2 lg:py-4">
+        <Link href="/">
+          <Image src="/logo.png" alt="logo" width={30} height={30} />
+        </Link>
+        <div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3.75 6A2.25 2.25 0 0 1 6 3.75h2.25A2.25 2.25 0 0 1 10.5 6v2.25a2.25 2.25 0 0 1-2.25 2.25H6a2.25 2.25 0 0 1-2.25-2.25V6ZM3.75 15.75A2.25 2.25 0 0 1 6 13.5h2.25a2.25 2.25 0 0 1 2.25 2.25V18a2.25 2.25 0 0 1-2.25 2.25H6A2.25 2.25 0 0 1 3.75 18v-2.25ZM13.5 6a2.25 2.25 0 0 1 2.25-2.25H18A2.25 2.25 0 0 1 20.25 6v2.25A2.25 2.25 0 0 1 18 10.5h-2.25a2.25 2.25 0 0 1-2.25-2.25V6ZM13.5 15.75a2.25 2.25 0 0 1 2.25-2.25H18a2.25 2.25 0 0 1 2.25 2.25V18A2.25 2.25 0 0 1 18 20.25h-2.25A2.25 2.25 0 0 1 13.5 18v-2.25Z"
+            />
+				  </svg>
+				  
+
+        </div>
+      </nav>
+    </header>
+  );
 }
+
---
+++ app/src/lib/constants.ts
@@ -0,0 +1,6 @@
+export const navLinks = [
+  { href: "/", key: "home", label: "Products" },
+  { href: "/", key: "gallery", label: "Gallery" },
+  { href: "/", key: "contact", label: "Contact" },
+  
+];
